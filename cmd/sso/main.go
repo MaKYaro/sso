de1d118dfc31afc31875aa9a823171e5b981f473
Misha Klyukin <klumisha@gmail.com>
2024-06-28 19:50:58 +0300

    setup logger

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,18 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"sso/internal/config"
+	"sso/internal/lib/logger/handlers/slogpretty"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
-	// TODO: init config
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
-	// TODO: init logger
+	log := setupLogger(cfg.Env)
+	log.Info("start app", slog.String("env", cfg.Env))
+	log.Debug("debug messager are enabled")
 
 	// TODO: init app
 
 	// TODO: run app
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(
+			os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelInfo},
+		))
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }

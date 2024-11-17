2eb4a395146339e0c78125d0a90b332aa25037d3
Misha Klyukin <klumisha@gmail.com>
2024-11-17 18:41:33 +0300

    implemented Login handler and request parameters' validation

--- go.mod
+++ go.mod
@@ -4,6 +4,9 @@ go 1.23.3
 
 require (
 	github.com/MaKYaro/protos v0.0.2
+	github.com/badoux/checkmail v1.2.4
+	github.com/wagslane/go-password-validator v0.3.0
+	google.golang.org/grpc v1.68.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
@@ -12,6 +15,5 @@ require (
 	golang.org/x/sys v0.25.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
-	google.golang.org/grpc v1.68.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 )
--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -4,23 +4,91 @@ import (
 	"context"
 
 	ssov1 "github.com/MaKYaro/protos/gen/go/sso"
+	"github.com/badoux/checkmail"
+	passwordvalidator "github.com/wagslane/go-password-validator"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Auth interface {
+	Login(
+		ctx context.Context,
+		email string,
+		password string,
+		appID int,
+	) (token string, err error)
+	RegisterNewUser(
+		ctx context.Context,
+		email string,
+		password string,
+	) (userID int64, err error)
+	IsAdmin(
+		ctx context.Context,
+		userID int64,
+	) (bool, error)
+}
 
 type serverAPI struct {
 	ssov1.UnimplementedAuthServer
+	auth Auth
 }
 
 // Register registers auth server
-func Register(gRPC *grpc.Server) {
-	ssov1.RegisterAuthServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, auth Auth) {
+	ssov1.RegisterAuthServer(gRPC, &serverAPI{auth: auth})
 }
+
+const (
+	emptyEmail     = ""
+	emptyAppID     = 0
+	minEntropyBits = 50
+)
 
 func (s *serverAPI) Login(
 	ctx context.Context,
 	req *ssov1.LoginRequest,
 ) (*ssov1.LoginResponse, error) {
-	panic("implement Login method")
+
+	if err := validateLogin(req); err != nil {
+		return nil, err
+	}
+
+	token, err := s.auth.Login(ctx, req.GetEmail(), req.GetPassword(), int(req.GetAppId()))
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.LoginResponse{
+		Token: token,
+	}, nil
+}
+
+func validateLogin(req *ssov1.LoginRequest) error {
+	email := req.GetEmail()
+
+	if email == emptyEmail {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+	if err := checkmail.ValidateFormat(email); err != nil {
+		return status.Error(codes.InvalidArgument, checkmail.ErrBadFormat.Error())
+	}
+	if err := checkmail.ValidateHost(email); err != nil {
+		return status.Error(codes.InvalidArgument, checkmail.ErrUnresolvableHost.Error())
+	}
+
+	password := req.GetPassword()
+	if err := passwordvalidator.Validate(password, minEntropyBits); err != nil {
+		return status.Error(codes.InvalidArgument, "weak password")
+	}
+
+	appID := req.GetAppId()
+
+	if appID == emptyAppID {
+		return status.Error(codes.InvalidArgument, "app_id required")
+	}
+
+	return nil
 }
 
 func (s *serverAPI) Register(

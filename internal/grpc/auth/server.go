b369348b130116c3ee30653c6317311c49098b55
Misha Klyukin <klumisha@gmail.com>
2024-11-20 21:27:50 +0300

    implemented Login, Register, IsAdmin handlers

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -41,6 +41,8 @@ func Register(gRPC *grpc.Server, auth Auth) {
 
 const (
 	emptyEmail     = ""
+	emptyPassword  = ""
+	emptyUserID    = 0
 	emptyAppID     = 0
 	minEntropyBits = 50
 )
@@ -64,7 +66,61 @@ func (s *serverAPI) Login(
 	}, nil
 }
 
+func (s *serverAPI) Register(
+	ctx context.Context,
+	req *ssov1.RegisterRequest,
+) (*ssov1.RegisterResponse, error) {
+	if err := validateRegister(req); err != nil {
+		return nil, err
+	}
+	userID, err := s.auth.RegisterNewUser(ctx, req.GetEmail(), req.GetPassword())
+
+	// TODO: handle service layer errors
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+
+	return &ssov1.RegisterResponse{
+		UserId: userID,
+	}, nil
+}
+
+func (s *serverAPI) IsAdmin(
+	ctx context.Context,
+	req *ssov1.IsAdminRequest,
+) (*ssov1.IsAdminResponse, error) {
+	if err := validateIsAdmin(req); err != nil {
+		return nil, err
+	}
+	isAdmin, err := s.auth.IsAdmin(ctx, req.GetUserId())
+
+	// TODO: implement handling of service layer error
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal erros")
+	}
+
+	return &ssov1.IsAdminResponse{
+		IsAdmin: isAdmin,
+	}, nil
+}
+
 func validateLogin(req *ssov1.LoginRequest) error {
+	if req.GetEmail() == emptyEmail {
+		return status.Error(codes.InvalidArgument, "email required")
+	}
+
+	if req.GetPassword() == emptyPassword {
+		return status.Error(codes.InvalidArgument, "password required")
+	}
+
+	if req.GetAppId() == emptyAppID {
+		return status.Error(codes.InvalidArgument, "app_id required")
+	}
+
+	return nil
+}
+
+func validateRegister(req *ssov1.RegisterRequest) error {
 	email := req.GetEmail()
 
 	if email == emptyEmail {
@@ -82,25 +138,12 @@ func validateLogin(req *ssov1.LoginRequest) error {
 		return status.Error(codes.InvalidArgument, "weak password")
 	}
 
-	appID := req.GetAppId()
-
-	if appID == emptyAppID {
-		return status.Error(codes.InvalidArgument, "app_id required")
-	}
-
 	return nil
 }
 
-func (s *serverAPI) Register(
-	ctx context.Context,
-	req *ssov1.RegisterRequest,
-) (*ssov1.RegisterResponse, error) {
-	panic("implement Register method")
-}
-
-func (s *serverAPI) IsAdmin(
-	ctx context.Context,
-	req *ssov1.IsAdminRequest,
-) (*ssov1.IsAdminResponse, error) {
-	panic("implement IsAdmin method")
+func validateIsAdmin(req *ssov1.IsAdminRequest) error {
+	if req.GetUserId() == emptyAppID {
+		return status.Error(codes.InvalidArgument, "user id required")
+	}
+	return nil
 }
